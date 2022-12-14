5ea45d30a940fb7c65e3d7ad5f13d69b346ccfa2
violetwhenisawu <anashape710@gmail.com>
2022-12-14 23:50:37 +0900

    feat: 게임 시작 메세지 출력 및 정답 배열 생성 기능 구현

--- 01-javascript-baseball/src/controller/Controller.js
+++ 01-javascript-baseball/src/controller/Controller.js
@@ -1,13 +1,26 @@
 const ComputerNum = require('../model/ComputerNum');
+const OutputView = require('../view/OutputView');
 
 class Controller {
   #ComputerNum;
+  #OutputView;
+  #RandomNum;
 
   constructor() {
     this.#ComputerNum = new ComputerNum();
+    this.#OutputView = new OutputView();
+    this.#RandomNum;
   }
 
-  start() {}
+  start() {
+    this.#OutputView.printGameStart();
+    this.startGame();
+  }
+
+  startGame() {
+    this.#RandomNum = this.#ComputerNum.getComputerNum();
+    console.log(this.#RandomNum);
+  }
 }
 
 module.exports = Controller;
--- 01-javascript-baseball/src/view/OutputView.js
+++ 01-javascript-baseball/src/view/OutputView.js
@@ -1,3 +1,9 @@
-class OutputView {}
+const { Console } = require('@woowacourse/mission-utils');
+
+class OutputView {
+  printGameStart() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+}
 
 module.exports = OutputView;
